db8ae565c64a11e547725f5ccf53c85c85dee858
subodhpun <subodhmagar07@gmail.com>
2024-11-21 15:41:54 +0545

    our products

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import client from '../../assets/images/client.jpg';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const Home = () => {
@@ -9,14 +13,53 @@ const Home = () => {
     {
       feedback: "This is amazing!",
       name: "John Doe",
-      designation: "CEO, Example Inc."
+      designation: "CEO, Example Inc.",
+      photo:client
+    },
+    {
+      feedback: "This is amazing!",
+      name: "John Doe",
+      designation: "CEO, Example Inc.",
+      photo:client
+    },
+    {
+      feedback: "This is amazing!",
+      name: "John Doe",
+      designation: "CEO, Example Inc.",
+      photo:client
+    },
+    {
+      feedback: "This is amazing!",
+      name: "John Doe",
+      designation: "CEO, Example Inc.",
+      photo:client
     },
     {
       feedback: "I love this product!",
       name: "Jane Smith",
-      designation: "Manager, Another Corp."
+      designation: "Manager, Another Corp.",
+      photo:client
     }
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    responsive: [
+          {
+            breakpoint: 768, // For smaller screens
+            settings: {
+              slidesToShow: 1, // Show only 1 slide
+            },
+          },
+        ],
+  };
 
   return (
     <div className='relative'>
@@ -32,15 +75,36 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Testimonial Section */}
-      {Testimonials.map((testimonial, index) => (
-        <div key={index}>
-           <p>{testimonial.feedback}</p>
-         </div>
-      ))}
-     
-    </div>
+      {/* Our Products */}
+      <div className="bg-gray-400">
+        <div className='flex flex-row font-custonFont space-x-1 justify-center'>
+          <h1 className='text-[3.5rem]'>Our</h1>
+          <h1 className='text-[3.5rem] text-customRed'>Products</h1>
+        </div>
+      </div>
 
+
+
+
+
+
+
+      {/* Testimonial Section */}
+      <h1 className='flex justify-center mt-8 font-custonFont text-4xl underline underline-offset-1'>Client's Testimonials</h1>
+      <div className='relative flex mt-14 justify-center overflow-x-hidden'>
+        {/* Testimonial Slider */}
+        <Slider {...settings} className="w-full shadow-lg border-2">
+          {Testimonials.map((testimonial, index) => (
+            <div key={index} className='p-6 m-4 rounded-lg w-auto flex flex-col items-center justify-center'>
+              <img src={testimonial.photo} alt='no img' className='rounded-full size-auto mx-auto mb-3' />
+              <p className='text-lg font-medium text-center'>{testimonial.feedback}</p>
+              <p className='text-sm text-gray-700 text-center'>{testimonial.designation}</p>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+    </div>
   );
 };
 
