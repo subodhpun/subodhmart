e52941bc1a2cd2b8af3d8ba4357d4be40bc6f7af
subodhpun <subodhmagar07@gmail.com>
2024-11-22 17:12:39 +0545

    products finished and catagories added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const navigate = useNavigate();
   const [products, setProducts]=useState([]);
   const [visibleCount, setVisibleCount] = useState(8); // Number of products to display initially
-
+  const [jewelerys, setJewelerys]=useState([])
 
     useEffect(() => {
       axios.get('https://fakestoreapi.com/products')
@@ -23,6 +23,17 @@ const Home = () => {
       .catch(error => {
         console.error('Error in data fetching:', error);
       });
+    }, []);
+
+    useEffect(()=>{
+      axios.get('https://fakestoreapi.com/products/category/jewelery')
+      .then(response =>{
+        console.log(response.data)
+        setJewelerys(response.data)
+      })
+      .catch(error=>{
+        console.log("error found", error);
+      })
     }, []);
 
     const handleShowMore = () => {
@@ -86,7 +97,7 @@ const Home = () => {
     <div className='relative'>
       {/* Hero Section */}
       <div className='w-full relative z-0'>
-        <img src="src\assets\images\slider-bg.jpg" alt="Background" />
+        <img src="src\assets\images\slider-bg.jpg" alt="Background" className='w-full object-cover'/>
         <div className='absolute z-10 justify-center items-start top-1/4 l-0 md:ml-36 flex flex-col px-8 transform animate-slide'>
           <h2 className='text-2xl sm:text-4xl lg:text-5xl font-bold font-custonFont text-customRed'>Welcome to</h2>
           <h1 className='text-3xl md:text-5xl lg:text-8xl font-extrabold font-custonFont text-customBlue'>Subodh Mart</h1>
@@ -97,22 +108,33 @@ const Home = () => {
       </div>
 
       {/* Our Products */}
-      <div className="">
-        <div className='flex flex-row font-custonFont space-x-1 justify-center'>
+      <div className="relative mt-20">
+       <div className='flex flex-col justify-center items-center'>
+       <div className='flex flex-row font-custonFont space-x-1 justify-center'>
           <h1 className='text-[3.5rem]'>Our</h1>
           <h1 className='text-[3.5rem] text-customRed'>Products</h1>
         </div>
+        <span className="w-1/6 h-6 bg-customRed [clip-path:polygon(0%_100%,50%_10%,100%_100%)]"></span>
+        </div>
         
-        <div className='grid grid-cols-4 gap-20'>
+        <div className='grid sm:grid-cols-2 md:gap-20 md:grid-cols-3 lg:grid-cols-4 m-10 bg-[rgb(255,255,255)]'>
         {products.slice(0, visibleCount).map((product, index) => (
-            <div key={index} className="relative p-5 bg-white rounded-lg shadow-md hover:shadow-xl size-[15rem]">
-              <img
+            <div key={index} className=" p-5 justify-center items-center ">
+             <div className='relative p-12 w-auto rounded-lg shadow-md hover:shadow-xl size-9/12 transform transition-transform duration-700 ease-in-out hover:scale-110 hover:overflow-hidden  justify-center items-center'>
+             <img
                 src={product.image}
                 alt={product.title}
-                className=""
+                className="size-full"
               />
-              <h3 className="text-center mt-2 font-semibold">{product.title}</h3>
+              <div className="bg-white bg-opacity-35 absolute inset-0 flex flex-col space-y-2 justify-center items-center text-white opacity-0  hover:opacity-100 ">
+                <h3 className='bg-customRed w-1/2 p-1 flex justify-center rounded-lg '>Add To Cart</h3>
+                <h3 className='bg-customBlue w-1/2 p-1 flex justify-center  rounded-lg'>Buy Now</h3>
+              </div>
+              </div>
+              <div className='relative border-2'>
+              <h3 className="text-center mt-2 font-semibold border-5">{product.title}</h3>
               <p className="text-center text-gray-700">${product.price}</p>
+              </div>
               </div>
           ))}
         </div>
@@ -122,16 +144,28 @@ const Home = () => {
         <div className="flex justify-center mt-8">
           <button
             onClick={handleShowMore}
-            className="px-6 py-3 bg-customBlue text-white font-bold rounded-lg hover:bg-customRed text-lg"
+              className="px-6 py-3 bg-customRed text-white rounded-lg transform  transition-colors ease-in-out duration-500 hover:bg-white hover:text-customRed hover:text-sm hover:border-2 hover:border-customRed"
           >
             Show More
           </button>
         </div>
       )}
 
+      {/* Shop by catagories container */}
+      <div className=' bg-blue-50'>
+        <div className='m-10 p-5'>
+          <h1 className='font-bold text-4xl '>Shop by Catagories</h1>
+        </div>
+        {/* electronics */}
+        <div className=''>
+         {jewelerys.map((jewelery, index)=>(
+          <div key={index}>
+            <img src={jewelery.iamge}/>
+          </div>
+         ))}
 
-
-
+        </div>
+      </div>
 
 
 
