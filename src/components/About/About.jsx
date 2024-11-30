09c94f7fa079cd6f162534132b4d625da1c03548
subodhpun <subodhmagar07@gmail.com>
2024-11-30 22:36:20 +0545

    about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,172 +1,46 @@
 import React from 'react';
 import Footer from '../Footer/Footer'; // Assuming you have a Footer component
-import styled from 'styled-components';
+import delivery from '../../../public/assets/images/delivery.png';
+import free from '../../../public/assets/images/free.png';
+import quality from '../../../public/assets/images/quality.png';
 
 const Pages = () => {
   return (
     <>
-      <Card />
+       <div className='flex justify-center p-14 text-4xl bg-customRed font-extrabold text-white text-[42px] text-[800]'>
+      <h1>About Us</h1>
+    </div>
+
+    <div className='flex flex-col m-8 p-4'>
+      <div>
+        <h1 className='flex justify-center font-custonFont text-4xl text-customBlue underline'>Why Shop With Us</h1>
+      </div>
+
+      {/* cards */}
+      <div className='flex flex-row justify-around m-12 gap-4'>
+      <cards className='bg-customBlue flex flex-col items-center p-5 rounded-md gap-4'>
+        <img src={delivery} alt='no img' className='size-40 invert'/>
+        <h2 className='text-white text-xl font-bold'>Fast Delivery</h2>
+      <p className='text-white w-10/12 text-justify'>Enjoy lightning-fast delivery with our reliable shipping services, ensuring your favorite products reach your doorstep quickly and hassle-free!</p>
+      </cards>
+
+      <cards className='bg-customBlue flex flex-col items-center p-5 rounded-md gap-4'>
+      <img src={free} alt='no img' className='size-40 invert'/>
+      <h2 className='text-white text-xl font-bold'>Free Shipping</h2>
+      <p className='text-white w-10/12 text-justify'>Take advantage of our free shipping service and enjoy a seamless shopping experience, with your favorite products delivered to your doorstep at no extra cost!</p>
+      </cards>
+
+      <cards className='bg-customBlue flex flex-col items-center p-5 rounded-md gap-4'>
+      <img src={quality} alt='no img' className='size-40 invert'/>
+      <h2 className='text-white text-xl font-bold'>Best Quality</h2>
+      <p className='text-white w-10/12 text-justify'>Discover the best quality products carefully selected to meet your highest expectations, ensuring satisfaction with every purchase you make!</p>
+      </cards>
+      </div>
+      
+    </div>
+
       <Footer />
     </>
   );
 }
-
-const Card = () => {
-  return (
-    <StyledWrapper>
-      <div className="main_wrapper">
-        <div className="main">
-          <div className="antenna">
-            <div className="antenna_shadow" />
-            <div className="a1" />
-            <div className="a1d" />
-            <div className="a2" />
-            <div className="a2d" />
-            <div className="a_base" />
-          </div>
-          <div className="tv">
-            <div className="cruve">
-              <svg
-                xmlSpace="preserve"
-                viewBox="0 0 189.929 189.929"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                className="curve_svg"
-              >
-                <path d="M70.343,70.343c-30.554,30.553-44.806,72.7-39.102,115.635l-29.738,3.951C-5.442,137.659,11.917,86.34,49.129,49.13 C86.34,11.918,137.664-5.445,189.928,1.502l-3.95,29.738C143.041,25.54,100.895,39.789,70.343,70.343z" />
-              </svg>
-            </div>
-            <div className="display_div">
-              <div className="screen_out">
-                <div className="screen_out1">
-                  <div className="screen">
-                    <span className="notfound_text">NOT FOUND</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="lines">
-              <div className="line1" />
-              <div className="line2" />
-              <div className="line3" />
-            </div>
-            <div className="buttons_div">
-              <div className="b1"><div /></div>
-              <div className="b2" />
-              <div className="speakers">
-                <div className="g1">
-                  <div className="g11" />
-                  <div className="g12" />
-                  <div className="g13" />
-                </div>
-                <div className="g" />
-                <div className="g" />
-              </div>
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="base1" />
-            <div className="base2" />
-            <div className="base3" />
-          </div>
-        </div>
-        <div className="text_404">
-          <div className="text_4041">4</div>
-          <div className="text_4042">0</div>
-          <div className="text_4043">4</div>
-        </div>
-      </div>
-    </StyledWrapper>
-  );
-}
-
-const StyledWrapper = styled.div`
-  .main_wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 30em;
-    height: 30em;
-  }
-
-  .main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 5em;
-  }
-
-  .antenna {
-    width: 5em;
-    height: 5em;
-    border-radius: 50%;
-    border: 2px solid black;
-    background-color: #f27405;
-    margin-bottom: -6em;
-    z-index: -1;
-  }
-
-  .antenna_shadow {
-    position: absolute;
-    background-color: transparent;
-    width: 50px;
-    height: 56px;
-    margin-left: 1.68em;
-    border-radius: 45%;
-    transform: rotate(140deg);
-    border: 4px solid transparent;
-    box-shadow: inset 0px 16px #a85103, inset 0px 16px 1px 1px #a85103;
-  }
-
-  .antenna::after {
-    content: "";
-    position: absolute;
-    margin-top: -9.4em;
-    margin-left: 0.4em;
-    transform: rotate(-25deg);
-    width: 1em;
-    height: 0.5em;
-    border-radius: 50%;
-    background-color: #f69e50;
-  }
-
-  .antenna::before {
-    content: "";
-    position: absolute;
-    margin-top: 0.2em;
-    margin-left: 1.25em;
-    transform: rotate(-20deg);
-    width: 1.5em;
-    height: 0.8em;
-    border-radius: 50%;
-    background-color: #f69e50;
-  }
-
-  .notfound_text {
-    background-color: black;
-    padding-left: 0.3em;
-    padding-right: 0.3em;
-    font-size: 0.75em;
-    color: white;
-    letter-spacing: 0;
-    border-radius: 5px;
-  }
-
-  .tv {
-    width: 17em;
-    height: 9em;
-    margin-top: 3em;
-    border-radius: 15px;
-    background-color: #d36604;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  /* Add more styles for your other elements as needed */
-`;
-
 export default Pages;
