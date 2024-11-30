221d3d9fe2a0aef3729c03d38c32274152466b0f
subodhpun <subodhmagar07@gmail.com>
2024-11-30 22:41:57 +0545

    responsivness of about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,13 +11,13 @@ const Pages = () => {
       <h1>About Us</h1>
     </div>
 
-    <div className='flex flex-col m-8 p-4'>
+    <div className='flex flex-col items-center m-8 p-4'>
       <div>
         <h1 className='flex justify-center font-custonFont text-4xl text-customBlue underline'>Why Shop With Us</h1>
       </div>
 
       {/* cards */}
-      <div className='flex flex-row justify-around m-12 gap-4'>
+      <div className='flex flex-col w-full md:flex-row justify-around m-12 gap-12 md:gap-4'>
       <cards className='bg-customBlue flex flex-col items-center p-5 rounded-md gap-4'>
         <img src={delivery} alt='no img' className='size-40 invert'/>
         <h2 className='text-white text-xl font-bold'>Fast Delivery</h2>
