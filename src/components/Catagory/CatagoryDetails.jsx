864a0e676a5c87b1dd131871262657868d3044e9
subodhpun <subodhmagar07@gmail.com>
2024-11-30 14:08:13 +0545

    cart added to product category

--- src/components/Catagory/CatagoryDetails.jsx
+++ src/components/Catagory/CatagoryDetails.jsx
@@ -28,11 +28,15 @@ const CategoryDetails = ({slider}) => {
       <h1 className='font-custonFont text-3xl md:text-4xl flex justify-center mt-8 md:m-8 underline'>Products In &nbsp; <span className='text-customRed first-letter:uppercase underline decoration-customRed'>{category}</span></h1>
       <div className="grid md:grid-cols-4 gap-20 p-4 m-20 shadow-sm">
         {products.map((product) => (
-          <div key={product.id} className="p-4 border rounded shadow-lg">
+          <div key={product.id} className="relative flex flex-col w-full p-4 rounded shadow-lg border-4 transition-all duration-700 ease-in-out hover:scale-110">
             <img src={product.image} alt={product.title} className="w-full h-40 object-contain" />
             <h2 className="text-lg font-bold">{product.title}</h2>
             <p className="text-sm">{product.description.substring(0, 100)}...</p>
             <p className="font-bold">${product.price}</p>
+            <div className="absolute h-full w-11/12 p-4 flex flex-col justify-center items-center text-white gap-2 opacity-0 transition-all duration-200 ease-in-out hover:opacity-100 hover:bg-white hover:bg-opacity-50 ">
+                <h3 className='bg-customRed w-1/2 p-1 flex justify-center rounded-lg '>Add To Cart</h3>
+                <h3 className='bg-customBlue w-1/2 p-1 flex justify-center  rounded-lg'>Buy Now</h3>
+              </div>
           </div>
         ))}
       </div>
